38af0a5ba83056bab09c97080296d25848ae70b6
LucasGarmen <lucasgarmendia95@gmail.com>
2026-05-23 19:18:58 -0300

    se volvio a modificar para que lea el favicon, ahora quedo funcionando

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.templatetags.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=static('images/favicon.png'), permanent=True)),
     path('admin/', admin.site.urls),
     path('', include('games.urls')),
     path('accounts/', include('accounts.urls')),
